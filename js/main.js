664ded557dc9d98106049eabc4c7c5f1ee34dd4a
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2019-04-29 17:20:53 -0400

    Moved scrolling to buttons

--- js/main.js
+++ js/main.js
@@ -1,33 +1,67 @@
+
 var width = d3.select("#streetview-containers").node().getBoundingClientRect().width;
+const mult = 148000; 
+var scroll = 0;
 
-const mult = 148000;
-
-var reels = [
-{'id': '#streetview-strip-holder-1966', 'src': '2012m1_ref199_241-north.csv'},
-    {'id': '#streetview-strip-holder-1973', 'src': '2012m1_ref32_4a7-north.csv'},
-    {'id': '#streetview-strip-holder-1985', 'src': '2012m1_ref67_835-north.csv'},
-    {'id': '#streetview-strip-holder-1995', 'src': '2012m1_ref79_8u9-north.csv'},
-    {'id': '#streetview-strip-holder-2007', 'src': '2012m2_ref33_oy0-north.csv'}
+const reels = [
+    {'year': '1966', 'src': '2012m1_ref199_241'},
+    {'year': '1973', 'src': '2012m1_ref32_4a7'},
+    {'year': '1985', 'src': '2012m1_ref67_835'},
+    {'year': '1995', 'src': '2012m1_ref79_8u9'},
+    {'year': '2007', 'src': '2012m2_ref33_oy0'}
     ];
 
+var streetviewContainers = d3.select('#streetview-containers').selectAll('div')
+    .data(reels)
+    .enter()
+    .append('div')
+    .attr('class','streetview-year-container')
+    .attr('id', function(d){ return 'streetview-year-container-' + d.year; });
 
-reels.forEach( buildStrip );
+streetviewContainers.append('div')
+    .attr('class','streetview-year-label')
+    .text(function(d){ return d.year; });
 
-function buildStrip(r) {
+var stripsContainers = streetviewContainers.append('div')
+    .attr('class','photography-strip-container')
+    .attr('id', function(d){ return 'photography-strip-container' + d.year; });
 
-    var svg = d3.select(r.id)
-        .append("svg")
-        .attr("width", width)
-        .attr("height", 260)
-        .attr("class", "strip-svg")
-        .call(d3.zoom().on("zoom",zoomed).on("end",zoomdraw));
+var strips = stripsContainers.append('svg')
+    .attr("width", width)
+    .attr("height", 250)
+    .attr("class", "strip-svg")
+    .attr("id", function(d) { return "strip-svg-" + d.year; });
 
-    var g = svg.append("g")
-        .attr('class','strip-g');
+strips.each(buildStrip);
 
-    d3.csv('./data/' + r.src).then(function(d){ loadItems(d,g); });
+var rightButton = d3.select("#scroll-button-right")
+    .on('click',function(){ scrollHandle(width*.8,"right"); });
 
- }
+var rightButton = d3.select("#scroll-button-left")
+    .on('click',function(){ scrollHandle(width*.8,"left"); });
+
+function buildStrip(d) {
+    var strip = d3.select(this);
+    d3.csv('./data/' + d.src + '-north.csv').then(function(d) { createWireframes(d,strip); });
+}
+
+function createWireframes(d,strip) {
+    var g = strip.append('g')
+        .attr("class","strip-g");
+
+    var rect = g.selectAll("rect")
+        .data(d)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return mult * parseFloat(d.index) })
+        .attr("y", "0")
+        .attr("width", "450")
+        .attr("height", "250")
+        .attr("class", "image-frame");
+
+    loadImages(g,0,width);
+}
+
 
 function loadItems(d,g) {
     g.selectAll("rect")
@@ -43,25 +77,24 @@ function loadItems(d,g) {
     loadImages(g,-10,width);
 }
     
-function zoomed() {
-    d3.selectAll('.strip-g').attr("transform","translate(" + d3.event.transform.x + ",0)");
-}
+// function zoomed() {
+//     d3.selectAll('.strip-g').attr("transform","translate(" + d3.event.transform.x + ",0)");
+// }
 
-function zoomdraw() {
+// function zoomdraw() {
 
-    var t = d3.event.transform.x;
-    var left = -50 - t;
-    var right = width - t;
+//     var t = d3.event.transform.x;
+//     var left = -50 - t;
+//     var right = width - t;
 
-    d3.selectAll('.strip-g')
-        .each(function(g) { loadImages(d3.select(this),left,right); } )    
-}
+//     d3.selectAll('.strip-g')
+//         .each(function(g) { loadImages(d3.select(this),left,right); } )    
+// }
 
 function loadImages(g,left,right) {
 
     g.selectAll("rect")
         .each( function(d) { 
-
             var x = d3.select(this).attr("x")
             if(x >= left && x <= right) { 
 
@@ -78,5 +111,18 @@ function loadImages(g,left,right) {
                 }
              }; 
         });
+}
+
+function scrollHandle(dist,dir) {
+
+    dir=='left' ? dist = scroll + dist : dist = scroll - dist;
+    scroll = dist;
+
+    d3.selectAll('.strip-g')
+        .transition()
+        .attr("transform","translate(" + dist + ",0)")
+        .each(function() { loadImages(d3.select(this),-scroll,width-scroll); });
+
+
 }
 
